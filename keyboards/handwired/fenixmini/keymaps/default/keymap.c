1b06b1e11efbfdb4881d0084b131f869dec72535
FenixAlive <luisangelmf@gmail.com>
2021-03-06 15:16:40 -0600

    original enter y space for tiling

--- keyboards/handwired/fenixmini/keymaps/default/keymap.c
+++ keyboards/handwired/fenixmini/keymaps/default/keymap.c
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_TAB        , KC_Q      , KC_D    , KC_R    , KC_F       , KC_B,
             KC_ESC        , KC_A      , KC_S    , KC_E    , KC_T       , KC_G,
             KC_LCTL       , KC_Z      , KC_X    , KC_M    , KC_C       , KC_V,
-            LSSPA         , TT(_TWO)  , ALTG    , GUI     , TT(_THREE) , KC_SFTENT,
+            KC_SFTENT     , TT(_TWO)  , ALTG    , GUI     , TT(_THREE) , LSSPA,
             KC_K          , KC_W      , KC_Y    , KC_P    , KC_SCLN    , KC_DEL,
             KC_H          , KC_N      , KC_I    , KC_O    , KC_U       , KC_QUOT,
             KC_J          , KC_L      , KC_COMM , KC_DOT  , KC_SLSH    , KC_LALT
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_TAB        , KC_Q      , KC_W    , KC_E    , KC_R       , KC_T,
             KC_ESC        , KC_A      , KC_S    , KC_D    , KC_F       , KC_G,
             KC_LCTL       , KC_Z      , KC_X    , KC_C    , KC_V       , KC_B,
-            LSSPA     , TT(_TWO)      , ALTG    , GUI     , TT(_THREE) , KC_SFTENT,
+            KC_SFTENT     , TT(_TWO)  , ALTG    , GUI     , TT(_THREE) , LSSPA,
             KC_Y          , KC_U      , KC_I    , KC_O    , KC_P       , KC_DEL,
             KC_H          , KC_J      , KC_K    , KC_L    , KC_SCLN    , KC_QUOT,
             KC_N          , KC_M      , KC_COMM , KC_DOT  , KC_SLSH    , KC_LALT
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_TAB        , KC_Q      , KC_W    , KC_E    , KC_R       , KC_T,
             KC_ESC        , KC_A      , KC_S    , KC_D    , KC_F       , KC_G,
             KC_LCTL       , KC_Z      , KC_X    , KC_C    , KC_V       , KC_B,
-            LSSPA     , TT(_TWO)      , ALTG    , GUI     , TT(_THREE) , KC_SFTENT,
+            KC_SFTENT     , TT(_TWO)  , ALTG    , GUI     , TT(_THREE) , LSSPA,
             KC_Y          , KC_U      , KC_I    , KC_O    , KC_P       , KC_BSPC,
             KC_H          , KC_J      , KC_K    , KC_L    , KC_SCLN    , KC_QUOT,
             KC_N          , KC_M      , KC_COMM , KC_DOT  , KC_SLSH    , KC_LALT
