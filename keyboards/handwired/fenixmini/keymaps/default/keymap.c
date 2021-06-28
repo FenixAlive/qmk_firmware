3b1a9e62673d0e3103ccd8a6470b99cd7416cabc
FenixAlive <luisangelmf@gmail.com>
2021-06-28 14:09:54 -0500

    cambio a layout 3 flechas

--- keyboards/handwired/fenixmini/keymaps/default/keymap.c
+++ keyboards/handwired/fenixmini/keymaps/default/keymap.c
@@ -44,9 +44,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             ),
     [_GAME] = LAYOUT (
             KC_TAB        , KC_Q       , KC_W    , KC_E    , KC_R       , KC_T,
-            KC_ESC       , KC_A       , KC_S    , KC_D    , KC_F       , KC_G,
+            KC_ESC        , KC_A       , KC_S    , KC_D    , KC_F       , KC_G,
             KC_LSFT       , KC_Z       , KC_X    , KC_C    , KC_V       , KC_B,
-            KC_SFTENT     , TT(_THREE) , GUI     , KC_LALT , TT(_GTWO) , KC_SPC,
+            KC_SFTENT     , MO(_THREE) , GUI     , KC_LALT , MO(_GTWO)  , KC_SPC,
             KC_Y          , KC_U       , KC_I    , KC_O    , KC_P       , KC_BSPC,
             KC_H          , KC_J       , KC_K    , KC_L    , KC_SCLN    , KC_QUOT,
             KC_N          , KC_M       , KC_COMM , KC_DOT  , KC_SLSH    , KC_LALT
@@ -70,22 +70,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_EQL         , KC_1      , KC_2      , KC_3      , KC_SLSH  , ALTMI
             ),
     [_THREE] = LAYOUT (
-            _______      , KC_PGUP    , KC_HOME  , KC_UP    , KC_END      , KC_BRIU,
-            TT(_NUMP)    , KC_LSFT    , KC_LEFT  , KC_DOWN  , KC_RGHT     , KC_BRID,
-            _______      , KC_PGDN    , KC_ACL0  , KC_ACL1  , KC_ACL2     , KC_CALC,
+            _______      , KC_HOME    , KC_UP    , KC_END   , KC_PGUP     , KC_BRIU,
+            TT(_NUMP)    , KC_LEFT    , KC_DOWN  , KC_RGHT  , KC_LSFT     , KC_BRID,
+            _______      , KC_ACL0    , KC_ACL1  , KC_ACL2  , KC_PGDN     , KC_CALC,
             KC_BTN1      , _______    , KC_BTN2  , _______  , _______     , _______,
             KC_VOLU      , KC_WH_U    , KC_MS_U  , KC_WH_D  , KC_BTN4     , KC_BTN5,
             KC_VOLD      , KC_MS_L    , KC_MS_D  , KC_MS_R  , KC_BTN3     , KC_PSCR,
-            KC_MUTE      , KC_MSEL    , KC_MPLY  , KC_MPRV  , KC_MNXT    , _______
+            KC_MUTE      , KC_MSEL    , KC_MPLY  , KC_MPRV  , KC_MNXT     , _______
             ),
     [_NUMP] = LAYOUT (
-            _______      , KC_PAST    , KC_PMNS  , KC_PPLS  , _______     , _______,
-            _______      , KC_PSLS    , KC_PDOT  , KC_PCMM  , _______     , _______,
+            _______      , _______    , KC_PAST  , KC_PMNS  , KC_PPLS     , _______,
+            _______      , _______    , KC_PSLS  , KC_PDOT  , KC_PCMM     , _______,
             _______      , _______    , _______  , _______  , _______     , _______,
             _______      , _______    , _______  , _______  , _______     , _______,
             KC_PSLS      , KC_KP_7    , KC_KP_8  , KC_KP_9  , KC_PAST     , KC_NLCK,
             KC_KP_0      , KC_KP_4    , KC_KP_5  , KC_KP_6  , KC_PMNS     , KC_PENT,
-            KC_PEQL      , KC_KP_1    , KC_KP_2  , KC_KP_3  , KC_PPLS     , KC_PDOT
+            KC_PDOT      , KC_KP_1    , KC_KP_2  , KC_KP_3  , KC_PPLS     , KC_PEQL 
             ),
     [_ADJUST] = LAYOUT (
             DM_PLY1      , KC_F1      , KC_F2      , KC_F3    , KC_F4     , _______,
